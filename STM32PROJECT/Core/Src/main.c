29bc574048e11127ece5332ed5ed5107eabf73a9
kiencantcode <103300252+kiencantcode@users.noreply.github.com>
2023-10-01 15:47:39 +0700

    update code ex2

--- STM32PROJECT/Core/Src/main.c
+++ STM32PROJECT/Core/Src/main.c
@@ -101,46 +101,50 @@ int main(void)
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, SET);
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, SET);
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, SET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, SET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, SET);
   while (1)
   {
     /* USER CODE END WHILE */
 	  if(timer1_flag == 1){
-		 switch(led){
-		 case 1:
-			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, RESET);
-			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, SET);
-			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, SET);
-			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, SET);
-			 display7SEG(led);
-			 led = 2;
-			 break;
-		 case 2:
-			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, SET);
-			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, RESET);
-			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, SET);
-			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, SET);
-			 display7SEG(led);
-			 led = 3;
-			 break;
-		 case 3:
-			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, SET);
-			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, SET);
-			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, RESET);
-			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, SET);
-			 display7SEG(led);
-			 led = 0;
-			 break;
-		 case 0:
-			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, SET);
-			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, SET);
-			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, SET);
-			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, RESET);
-			 display7SEG(led);
-			 led = 1;
-			 break;
-		 }
-		 setTimer1(50);
-	  }
+		  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_4);
+		  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+	 		 switch(led){
+	 		 case 1:
+	 			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, RESET);
+	 			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, SET);
+	 			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, SET);
+	 			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, SET);
+	 			 display7SEG(led);
+	 			 led = 2;
+	 			 break;
+	 		 case 2:
+	 			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, SET);
+	 			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, RESET);
+	 			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, SET);
+	 			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, SET);
+	 			 display7SEG(led);
+	 			 led = 3;
+	 			 break;
+	 		 case 3:
+	 			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, SET);
+	 			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, SET);
+	 			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, RESET);
+	 			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, SET);
+	 			 display7SEG(led);
+	 			 led = 0;
+	 			 break;
+	 		 case 0:
+	 			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, SET);
+	 			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, SET);
+	 			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, SET);
+	 			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, RESET);
+	 			 display7SEG(led);
+	 			 led = 1;
+	 			 break;
+	 		 }
+	 		 setTimer1(50);
+	 	  }
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -240,17 +244,17 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8
-                          |GPIO_PIN_9, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7
+                          |GPIO_PIN_8|GPIO_PIN_9, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
                           |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PA5 PA6 PA7 PA8
-                           PA9 */
-  GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8
-                          |GPIO_PIN_9;
+  /*Configure GPIO pins : PA4 PA5 PA6 PA7
+                           PA8 PA9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7
+                          |GPIO_PIN_8|GPIO_PIN_9;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
